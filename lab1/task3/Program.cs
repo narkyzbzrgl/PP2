b1bef7b4ef21567e51fe78692c8a22284ed816b2
Narkyz <narararok@gmail.com>
2019-02-15 19:35:55 +0600

    comment

---
+++ lab1/task3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());          //переменная, и сохраняем в ней кол-во чисел которые вводим
+            string[] arr = Console.ReadLine().Split();      //массив строк, считаем числы и разделяем их как отдельный элемент
+            int[] a = new int[n * 2];                      // увеличиваем размер массива в два раза
+            int k = 0;                                     // нужна чтобы пользоваться ею при заполнении чисел в массив
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    a[k] = int.Parse(arr[i]);               //заполнение массива с повторяющимися цифрами
+                    k++;
+                }
+            }
+            for (int i = 0; i < n * 2; i++)
+            {
+                Console.Write(a[i] + " ");                 //выводит на консоль
+            }
+        }
+    }
+}
---
+++ lab1/task4/task4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());        //переменная и сохранение в него размер массива
+            string[,] arr = new string[n, n];             //создание двумерного массива
+            for (int i = 0; i < n; i++)                       //заполнение массива [*]
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    arr[i, j] += "[*]";
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i >= j)                           //заполнение на половину
+                        Console.Write(arr[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
